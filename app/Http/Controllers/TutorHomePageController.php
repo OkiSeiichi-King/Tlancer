ea593ab569f2d5f73f2e1ffe9f753cb605677a8d
snowlancer9 <topstardev.703@gmail.com>
2022-09-14 21:04:08 +0100

    tutor courses and new courses page

--- app/Http/Controllers/TutorHomePageController.php
+++ app/Http/Controllers/TutorHomePageController.php
@@ -10,4 +10,12 @@ class TutorHomePageController extends Controller
     public function index(){
         return inertia('ProfilePage/TutorPage/TutorPage');
     }
+
+    public function courses(){
+        return inertia('ProfilePage/TutorPage/MyCourse');
+    }
+
+    public function create(){
+        return inertia('ProfilePage/TutorPage/NewCourse');
+    }
 }
